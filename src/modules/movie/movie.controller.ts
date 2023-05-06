691d1ef86392a0d891eac0187d76829bf7a6dfe0
Geodor Ruales <geodorruales@gmail.com>
2023-05-06 23:08:03 +0800

    feat: add crud to movie controller

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -1,9 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { InsertMovieDto } from './dto/insertMovie.dto';
+import { UUID } from 'crypto';
 
 @Controller('movie')
 export class MovieController {
+  constructor(private movieService: MovieService) {}
+
   @Get()
-  getAllMovies() {
-    return 'Return all movies';
+  async getAllMovies() {
+    return await this.movieService.getAllMovies();
+  }
+
+  @Get(':id')
+  async findMovieById(@Param() { id }: { id: UUID }) {
+    return await this.movieService.findMovieById(id);
+  }
+
+  @Post()
+  async saveMovie(@Body() saveMovieDto: InsertMovieDto) {
+    return await this.movieService.saveMovie(saveMovieDto);
+  }
+
+  @Patch(':id')
+  async updateMovieById(
+    @Param() { id }: { id: UUID },
+    @Body() updateMovieDto: InsertMovieDto,
+  ) {
+    return await this.movieService.updateMovieById(
+      await this.movieService.findMovieById(id),
+      updateMovieDto,
+    );
+  }
+
+  @Delete(':id')
+  async removeMovieById(@Param() { id }: { id: UUID }) {
+    return await this.movieService.removeMovieById(
+      await this.movieService.findMovieById(id),
+    );
+  }
+
+  @Delete()
+  async removeAllMovies() {
+    return await this.movieService.removeAllMovies(
+      await this.movieService.getAllMovies(),
+    );
   }
 }
