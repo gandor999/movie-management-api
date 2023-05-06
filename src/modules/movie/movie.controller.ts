1f810c718434f1b2595fff6388e87e45d6fba936
Geodor Ruales <geodorruales@gmail.com>
2023-05-06 19:34:02 +0800

    feat: add placeholder to return all movies

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+  @Get()
+  getAllMovies() {
+    return 'Return all movies';
+  }
+}
