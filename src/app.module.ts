68ee28ef2e46318a82c8cb2e6b094dd8cf57fce0
Geodor Ruales <geodorruales@gmail.com>
2023-05-06 19:18:52 +0800

    feat: add movie controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MovieModule } from './movie/movie.module';
+import { MovieModule } from './modules/movie/movie.module';
 
 @Module({
   imports: [MovieModule],
